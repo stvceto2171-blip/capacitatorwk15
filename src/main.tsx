5dd70fe145cae031f66880abed38ff33ec5c118e
stvceto2171-blip <stvceto2171@gmail.com>
2025-11-28 16:44:52 -0800

    ok1

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+// CHANGE: Add the following import
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+
+// CHANGE: Call the element loader before the render call
+defineCustomElements(window);
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
