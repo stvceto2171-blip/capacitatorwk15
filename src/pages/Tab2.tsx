4d384f0cfbf31a2a6cf1068f8c81f25679055fdb
stvceto2171-blip <stvceto2171@gmail.com>
2025-11-29 17:47:48 -0800

    ok3

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,5 +1,18 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { camera } from 'ionicons/icons';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+} from '@ionic/react';
+
+// CHANGE: Remove or comment out ExploreContainer
+// import ExploreContainer from '../components/ExploreContainer';
+
 import './Tab2.css';
 
 const Tab2: React.FC = () => {
@@ -7,16 +20,26 @@ const Tab2: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Photo Gallery</IonTitle>
         </IonToolbar>
       </IonHeader>
+
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
+            <IonTitle size="large">Photo Gallery</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+
+        {/* Floating Action Button with Camera Icon */}
+        <IonFab vertical="bottom" horizontal="center" slot="fixed">
+          <IonFabButton>
+            <IonIcon icon={camera} />
+          </IonFabButton>
+        </IonFab>
+
+        {/* ExploreContainer has been removed */}
+        {/* <ExploreContainer name="Tab 2 page" /> */}
       </IonContent>
     </IonPage>
   );
